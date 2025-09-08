977e4bc72ec9a3675492cfc512ce394d8b6ba3b9
rwilliams175 <rwilliams175@phm.k12.in.us>
2025-09-08 09:48:31 -0400

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,17 @@
+#LISTS
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[0])
+print(nums[-1])
+print(len(nums)/2)
+nums[2] = 99
+print(nums)
+even_list = []
+for number in nums:
+    if number % 2 == 0:
+        even_list.append(number)
+print(even_list)
+#DICTIORNARIES
+countries = {'India':'New Delhi', 'China':'Beijing','United States':'Washington DC','Indonesia':'Jakarta','Pakistan':'Islamabad'}
+#SETS
+
+#TUPLES
