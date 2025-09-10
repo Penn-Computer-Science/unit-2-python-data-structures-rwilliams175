f1c96dc70885c50dc8b2cb275e1afc9be111e435
rwilliams175 <rwilliams175@phm.k12.in.us>
2025-09-10 09:48:27 -0400

    Finished Data Structures, Still Working on Gradebook

--- DataStructures.py
+++ DataStructures.py
@@ -12,6 +12,22 @@ for number in nums:
 print(even_list)
 #DICTIORNARIES
 countries = {'India':'New Delhi', 'China':'Beijing','United States':'Washington DC','Indonesia':'Jakarta','Pakistan':'Islamabad'}
+print(countries['India'])
+print(countries['China'])
+countries['Nigeria'] = 'Abuja'
+countries['Brazil'] = 'Brasilia'
+countries['United States'] = 'Indianapolis'
+print(countries)
 #SETS
-
+fruits = {'apple', 'mango', 'banana', 'watermelon', 'orange'}
+other_fruits = {'pear', 'grapes'}
+fruits.update(other_fruits)
+friend_fruits = {'pineapple', 'banana', 'dragonfruit', 'watermelon', 'apple', 'cherry', 'kiwi'}
+print (fruits & friend_fruits)
+print(fruits - friend_fruits)
+print(fruits | friend_fruits)
 #TUPLES
+colors = ('red', 'green', 'blue', 'yellow', 'purple')
+print(colors[0] + " " + colors[-1])
+print(colors)
+colors[0] = 'pink'
--- gradebookManager.py
+++ gradebookManager.py
@@ -1,2 +1,23 @@
+global new_gradebook
+new_grade = []
+roman_grades = [96, 93, 87]
+adam_grades = [100, 97, 78]
+edin_grades = [67, 77, 82]
+gradebook = {'Roman': roman_grades, 'Adam': adam_grades, 'Edin': edin_grades}
+def add_student():
+    new_grade.clear()
+    global new_gradebook
+    new_student_name = input("Enter name: ")
+    for i in range(3):
+        new_student_grade = input("Enter grade:")
+        new_student_grade = int(new_student_grade)
+        new_grade.append(new_student_grade)
+    new_students = {new_student_name:new_grade}
+    new_gradebook = gradebook | new_students
+add_student()
+print(new_gradebook)
+add_student()
+print(new_gradebook)
 
-#change
+
+
