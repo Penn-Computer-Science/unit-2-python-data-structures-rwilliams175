96be6c942770fd9bb4f9a69ee8ad938e640fbdec
rwilliams175 <rwilliams175@phm.k12.in.us>
2025-09-22 09:50:38 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+b_names = ['Noah', 'Liam', 'Jacob', 'Williams', 'Mason', 'Ethan', 'Michael', 'Alexander', 'James', 'Elijah']
+b_freq = [183330, 173981, 163266, 159945, 157875, 149082, 145171, 142142, 139652, 137093]
+g_names = ['Emma', 'Olivia', 'Sophia', 'Isabella', 'Ava', 'Mia', 'Abigail', 'Emily', 'Charlotte', 'Madison']
+g_freq = [195028, 184528, 181132, 170559, 155844, 129088, 118713, 117626, 102470, 98419]
+df = pd.DataFrame{
+    (
+        "Boys Names":b_names, 
+        "B Frequency":b_freq, 
+        "Girls Names":g_names,
+        "G Frequency":g_freq
+    )
+}
+print(df)
+print [df.describe()]
